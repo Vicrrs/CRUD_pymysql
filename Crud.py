184f6fa26c3ec89d9b22027f46c00946ab9d1960
VICTOR ROZA SOUZA <enricofermi2019@gmail.com>
2022-11-25 15:23:32 -0400

    Inserindo clientes

--- Crud.py
+++ Crud.py
@@ -1,19 +1,51 @@
 import pymysql.cursors
 import pymysql
+from contextlib import contextmanager
 
-conexao = pymysql.Connect(
-    host='127.0.0.1',
-    user='root',
-    password='',
-    charset='utf8mb4',
-    db='clientes',
-    cursorclass=pymysql.cursors.DictCursor
-)
 
-with conexao.cursor() as cursor:
-    cursor.execute('SELECT * FROM clientes')
-    resultado = cursor.fetchall()
+@contextmanager
+def conecta():
+    conexao = pymysql.Connect(
+        host='127.0.0.1',
+        user='root',
+        password='',
+        charset='utf8mb4',
+        db='clientes',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    try:
+        yield conexao
+    finally:
+        print('Conexão fechada')
+        conexao.close()
 
-    print(resultado)
 
-conexao.close()
+# Vizualizando as modificações no banco
+# with conecta() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'INSERT INTO clientes (nome, sobrenome, idade, peso) VALUES (%s, %s, %s, %s)'
+#         cursor.execute(sql, ('João', 'Sena', 24, 69))
+#         conexao.commit()
+
+# inserindo vários clientes
+# with conecta() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'INSERT INTO clientes (nome, sobrenome, idade, peso) VALUES (%s, %s, %s, %s)'
+#         dados = [
+#             ('Nikola', 'tesla', '29', '70'),
+#             ('Isaac', 'Newton', '24', '69'),
+#             ('Albert', 'Eisntein', '33', '80')
+#         ]
+#         # varios valores
+#         cursor.executemany(sql, dados)
+#         conexao.commit()
+
+# fechando a conexao
+with conecta() as conexao:
+    # fechando o cursor
+    with conexao.cursor() as cursor:
+        cursor.execute('SELECT * FROM clientes ORDER BY ID ASC LIMIT 100 ')
+        resultado = cursor.fetchall()
+
+        for linha in resultado:
+            print(linha)
